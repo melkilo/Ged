282a053c1a070827dfa78f192d9b39c7b227a9bb
BENIDAL <abenidal@dxc.com>
2020-05-02 17:12:05 +0000

    Customization swagger APi

--- Ged-ws/src/main/java/org/Ged/ws/api/client/ClientApi.java
+++ Ged-ws/src/main/java/org/Ged/ws/api/client/ClientApi.java
@@ -16,39 +16,52 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping(value = "/api/ged")
+@Api(description = "CRUD API Client")
 public class ClientApi {
 
 	@Autowired
 	private ClientServiceSilo clientServiceSilo;
-	
+
 	@GetMapping("/getAllClients")
+    @ApiOperation(value = "Get all clients from DB !")
+
 	public List<ClientDto> getAllClients() {
 		return clientServiceSilo.findAllClients();
 	}
-	
+
+   
+
 	@GetMapping("/getClientById/{idClient}")
+	@ApiOperation(value = "Get client by id from DB !")
 	public ClientDto getClientById(@PathVariable Long idClient) {
 		return clientServiceSilo.findClientById(idClient);
 	}
-	
+
 	@GetMapping("/search")
+	@ApiOperation(value = "Get all clients from ES !")
 	public Page<ClientDto> search() {
 		return clientServiceSilo.search();
 	}
 
 	@PostMapping("/addClient")
+	@ApiOperation(value = "Add new client  !")
 	public ClientDto addClients(@RequestBody ClientDto client) {
 		return clientServiceSilo.saveClient(client);
 	}
 
 	@DeleteMapping("/deleteClient/{id}")
+	@ApiOperation(value = "Delete client !")
 	public void deleteClient(@PathVariable Long id) {
 		clientServiceSilo.deleteClient(id);
 	}
 
 	@PutMapping("/updateClient")
+	@ApiOperation(value = "Update client !")
 	public void updateClient(@RequestBody ClientDto client) {
 		clientServiceSilo.updateClient(client);
 	}
